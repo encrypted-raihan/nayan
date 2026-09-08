5b240ea41f190268db8e8cadc6602d58c23dfb38
Raihan Shiras <raihanshiras08@gmail.com>
2026-09-08 22:08:51 +0530

    Register satellite provider in NAYAN data registry

--- lib/data/registry.ts
+++ lib/data/registry.ts
@@ -1,9 +1,11 @@
+import { celestrakActiveSatelliteProvider } from "./satellites";
 import { eonetNaturalEventsProvider } from "./providers/eonet-natural-events";
 import { usgsEarthquakeProvider } from "./providers/usgs-earthquakes";
 
 export const nayanProviders = {
   earthquakes: usgsEarthquakeProvider,
   "natural-events": eonetNaturalEventsProvider,
+  satellites: celestrakActiveSatelliteProvider,
 } as const;
 
 export type NayanProviderKey = keyof typeof nayanProviders;
