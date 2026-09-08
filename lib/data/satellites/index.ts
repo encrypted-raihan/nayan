199631e49757d090f18318f33a4b3735617eb692
Raihan Shiras <raihanshiras08@gmail.com>
2026-09-08 22:08:40 +0530

    Expose NAYAN satellite data API

---
+++ lib/data/satellites/index.ts
@@ -0,0 +1,3 @@
+export type { NayanSatellite } from "./types";
+export type { NayanSatelliteRecord } from "./provider";
+export { celestrakActiveSatelliteProvider, propagateSatellite } from "./provider";
