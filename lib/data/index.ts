296f7ee64195a2d9c01247b33e7b36aad8a16ee8
Raihan Shiras <raihanshiras08@gmail.com>
2026-09-08 21:42:35 +0530

    Add stable NAYAN data-layer public API

---
+++ lib/data/index.ts
@@ -0,0 +1,4 @@
+export { nayanDataEngine, NayanDataEngine } from "./engine";
+export { nayanProviders } from "./registry";
+export type { NayanDataKey, NayanEarthquake, NayanNaturalEvent, NaturalEventCategory } from "./types";
+export type { NayanDataProvider, NayanDataResult } from "./provider";
