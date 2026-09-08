bfcd32aa827a79771cb6ce8cccccbc9cded858ee
Raihan Shiras <raihanshiras08@gmail.com>
2026-09-08 22:40:48 +0530

    Remove temporary satellite index duplicate

--- lib/data/satellites/index2.ts
+++
@@ -1,4 +0,0 @@
-export type { NayanSatellite } from "./types";
-export type { NayanSatelliteRecord } from "./provider";
-export { celestrakActiveSatelliteProvider, propagateSatellite } from "./provider";
-export { fetchActiveSatellites } from "./loader";
