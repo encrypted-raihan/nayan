a907c8773b14c48190f6a591886e92e5ab4489f3
Raihan Shiras <raihanshiras08@gmail.com>
2026-09-08 22:50:06 +0530

    Export SatNOGS satellite provider

--- lib/data/satellites/index.ts
+++ lib/data/satellites/index.ts
@@ -1,3 +1,3 @@
 export type { NayanSatellite, NayanSatelliteCategory } from "./types";
 export type { NayanSatelliteRecord } from "./provider";
-export { celestrakActiveSatelliteProvider, fetchActiveSatellites, propagateSatellite } from "./provider";
+export { satnogsActiveSatelliteProvider, fetchActiveSatellites, propagateSatellite } from "./provider";
