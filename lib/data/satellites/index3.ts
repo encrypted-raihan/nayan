47c387e1c3cbc96400bf861eba01255c310525c8
Raihan Shiras <raihanshiras08@gmail.com>
2026-09-08 22:10:35 +0530

    Add satellite exports

---
+++ lib/data/satellites/index3.ts
@@ -0,0 +1,4 @@
+export type { NayanSatellite } from "./types";
+export type { NayanSatelliteRecord } from "./provider";
+export { celestrakActiveSatelliteProvider, propagateSatellite } from "./provider";
+export { fetchActiveSatellites } from "./loader";
