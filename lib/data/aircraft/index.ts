070bca6f451bc63bf8757f5a390220c003a4254b
Raihan Shiras <raihanshiras08@gmail.com>
2026-09-09 19:52:38 +0530

    Export aircraft data module

---
+++ lib/data/aircraft/index.ts
@@ -0,0 +1,3 @@
+export type { AircraftCategory, NayanAircraft } from "./types";
+export { fetchIndiaAircraft, adsbLolAircraftProvider } from "./provider";
+export { AIRCRAFT_QUERY_CENTERS, AIRCRAFT_QUERY_RADIUS_NM, AIRCRAFT_REFRESH_MS, AIRCRAFT_REGION } from "./config";
