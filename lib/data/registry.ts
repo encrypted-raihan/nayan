6e7da5c03e292d330bc6b7c17767967964d579a6
Raihan Shiras <raihanshiras08@gmail.com>
2026-09-10 20:26:30 +0530

    Switch satellite registry to SatNOGS

--- lib/data/registry.ts
+++ lib/data/registry.ts
@@ -1,11 +1,11 @@
-import { celestrakActiveSatelliteProvider } from "./satellites";
+import { satnogsActiveSatelliteProvider } from "./satellites";
 import { eonetNaturalEventsProvider } from "./providers/eonet-natural-events";
-import { usgsEarthquakeProvider } from "./providers/usgs-earthquakes";
+import { usgsEarthquakeProvider } from "./providers/providers/usgs-earthquakes";
 
 export const nayanProviders = {
   earthquakes: usgsEarthquakeProvider,
   "natural-events": eonetNaturalEventsProvider,
-  satellites: celestrakActiveSatelliteProvider,
+  satellites: satnogsActiveSatelliteProvider,
 } as const;
 
 export type NayanProviderKey = keyof typeof nayanProviders;
