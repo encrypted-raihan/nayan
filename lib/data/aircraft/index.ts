1dfb72f6c2f6dd68bef9a4d82e26a55193c04a3c
Raihan Shiras <raihanshiras08@gmail.com>
2026-09-13 06:56:32 +0530

    Wire traffic controller into globe client

--- lib/data/aircraft/index.ts
+++ lib/data/aircraft/index.ts
@@ -1,3 +1,5 @@
+import "../traffic/controller";
+
 export type { AircraftCategory, NayanAircraft } from "./types";
 export { fetchIndiaAircraft, adsbLolAircraftProvider } from "./provider";
 export { AIRCRAFT_QUERY_CENTERS, AIRCRAFT_QUERY_RADIUS_NM, AIRCRAFT_REFRESH_MS, AIRCRAFT_REGION } from "./config";
